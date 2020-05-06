17ddab2d9c0b0c24e9d94e71698e7fb835fe1c3d
ratna <ratnasrinivas1992@gmail.com>
2020-05-06 15:21:06 +0530

    git all http methods and reactive form example

--- angular-practice/src/app/httpservices/api.service.ts
+++ angular-practice/src/app/httpservices/api.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { HttpClient,HttpHeaders,HttpParams  } from '@angular/common/http';
 import { SmartPhone } from '../interfaces/SmartPhone';
 import { Observable } from 'rxjs';
 
 const getSmartPhones = 'http://localhost:8080/getSmartPhonesList';
+const postSmartPhones='http://localhost:8080/saveSmartPhone';
+const putSmartPhones='http://localhost:8080/updateSmartPhone';
+const deleteSmartPhones='http://localhost:8080/deleteSmartPhone';
 @Injectable({
   providedIn: 'root'
 })
@@ -22,8 +25,36 @@ export class ApiService {
 
   //post API
 
+  saveSmartPhones(smartPhone:SmartPhone): Observable<SmartPhone[]> {
+    let headers = new HttpHeaders().set('Access-Control-Allow-Origin', "*");
+    headers = headers.append('Content-Type', 'application/json; charset=utf-8');
+    return this.httpClient.post<SmartPhone[]>(postSmartPhones,smartPhone,{headers:headers});
+  }
 
+//put API
 
+  updateSmartPhones(id:string,price:Number): Observable<SmartPhone> {
+    let headers = new HttpHeaders().set('Access-Control-Allow-Origin', "*");
+    headers = headers.append('Content-Type', 'application/json; charset=utf-8');
+    let body = new HttpParams();
+    body = body.set('id', id);
+    body = body.set('price', price+"");
+    return this.httpClient.put<SmartPhone>(postSmartPhones,body,{headers:headers});
+  }
+
+  //put API
+
+  deleteSmartPhones(id:string): Observable<SmartPhone[]> {
+    let headers = new HttpHeaders().set('Access-Control-Allow-Origin', "*");
+    headers = headers.append('Content-Type', 'application/json; charset=utf-8');
+    let body = new HttpParams();
+    body = body.set('id', id);
+    const httpOptions = {
+    headers: headers,
+    params: body
+};
+    return this.httpClient.delete<SmartPhone[]>(deleteSmartPhones,httpOptions);
+  }
 
 
 }
